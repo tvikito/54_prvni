a350a2a0655e0000a0e203b6b01e25e5d8e3203c
Tomáš Vykoukal <vykoukal.tom@gmail.com>
2018-03-30 01:38:44 +0200

    Comit před testováním Buddy

--- contact.php
+++ contact.php
@@ -3,12 +3,12 @@
 include_once 'templates.php';
 
 $sablona = "onecolumn";
-$pagetitle = "První stránka";
+$pagetitle = "Write me! Yes!";
 $obsah = '
 <img class="img-ja" src="img/ja_100.jpg" srcset="img/ja_100.jpg, img/ja_200.jpg" width="100" height="100" alt="Tomáš Vykoukal">
-<h1>Papa of your UX and frontend code</h1>
+<h1>Contact</h1>
 <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur bibendum justo non orci. Vestibulum fermentum tortor id mi.</p>';
 
-template_home($pagetitle, $obsah);
+template_contact($pagetitle, $obsah);
 
 ?>
