d7cbf2dbf30ed9e6d8c6ecd025b5d3172305d4bd
Tomáš Vykoukal <vykoukal.tom@gmail.com>
2018-03-29 20:02:31 +0200

    Drobné úpravy

--- index.php
+++ index.php
@@ -5,9 +5,7 @@ include_once 'template_home.php';
 $sablona = "onecolumn";
 $pagetitle = "První stránka";
 $obsah = '
-<img class="img-ja" src="img/ja_100.jpg"
-  srcset="img/ja_100.jpg, img/ja_200.jpg"
-  width="100" height="100" alt="Tomáš Vykoukal">
+<img class="img-ja" src="img/ja_100.jpg" srcset="img/ja_100.jpg, img/ja_200.jpg" width="100" height="100" alt="Tomáš Vykoukal">
 <h1>Papa of your UX and frontend code</h1>
 <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur bibendum justo non orci. Vestibulum fermentum tortor id mi.</p>';
 
