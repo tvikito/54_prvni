ea053ab31ecbbcd8a548f42b9c817b3af2ee763b
Tomáš Vykoukal <vykoukal.tom@gmail.com>
2018-03-29 19:15:30 +0200

    Final snad

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
 
-include_once 'templates.php';
+include_once 'template_home.php';
 
 $sablona = "onecolumn";
 $pagetitle = "První stránka";
 $obsah = '
-<img class="img-ja" src="img/ja_orig.jpg">
+<img class="img-ja" src="img/ja_100.jpg"
+  srcset="img/ja_100.jpg, img/ja_200.jpg"
+  width="100" height="100" alt="Tomáš Vykoukal">
 <h1>Papa of your UX and frontend code</h1>
 <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur bibendum justo non orci. Vestibulum fermentum tortor id mi.</p>';
 
--- template_home.php
+++ template_home.php
@@ -35,11 +35,11 @@ function headerOneColumn($title, $obsah) {
             <ul>
                 <li><a href="/">Home</a></li>
                 <li><a href="/about">About us</a>
-                <ul>
-                    <li><a href="/about/history">History</a></li>
-                    <li><a href="/about/team">The team</a></li>
-                    <li><a href="/about/address">Our address</a></li>
-                </ul>
+                    <ul>
+                        <li><a href="/about/history">History</a></li>
+                        <li><a href="/about/team">The team</a></li>
+                        <li><a href="/about/address">Our address</a></li>
+                    </ul>
                 </li>
                 <li><a href="/contact">Contact</a></li>
             </ul>
@@ -47,15 +47,15 @@ function headerOneColumn($title, $obsah) {
 
         <div id="page">
             <div id="darker"></div>
-            <div class="container flex flex-column center">
-                <header class="flex flex_align--center padding-25" id="header">
-                    <div class="margin_right--auto" id="logo">
+            <div class="container h-100 flex flex-column center">
+                <header class="sm-d-flex flex_align--center padding-15" id="header">
+                    <div class="margin_right--auto padding-tb-10" id="logo">
                         <a href="/">
                             <img class="inline-block-center" src="img/tomas-vykoukal.png" alt="Tomáš Vykoukal">
                             <h1 class="inline-block-center">Tomáš Vykoukal</h1>
                         </a>
                     </div>
-                    <div class="padding-lr-25" id="menu-toogle" >
+                    <div class="padding-tb-10" id="menu-toogle" >
                         <div id="nav-icon3">
                             <span></span>
                             <span></span>
@@ -64,11 +64,11 @@ function headerOneColumn($title, $obsah) {
                         </div>
                     </div>
                 </header>
-                <article class="row margin_left-right--auto margin_top-bottom--auto padding-25" id="main-content-front">
+                <article class="row margin_left-right--auto margin_top-bottom--auto padding-15 padding-tb-20" id="main-content-front">
                         <?php echo $obsah; ?>
                 </article>
-                <footer class="padding-lr-25" role="contentinfo">
-                    <p>© <?php echo date("Y"); ?> by <abbr title="papa means Italian father">papa</abbr> Tomas</p>
+                <footer class="padding-10" role="contentinfo">
+                    <p>© <?php echo date("Y"); ?> created with my youngest sister and oldest granma by <abbr title="papa means Italian father">papa</abbr> Tomas</p>
                 </footer>
             </div>
         </div>
