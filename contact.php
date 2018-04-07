a009df51904420c61c1acef0f385201b0932030d
Tomáš Vykoukal <vykoukal.tom@gmail.com>
2018-04-07 14:01:11 +0200

    Úprava HTML šablony

--- contact.php
+++ contact.php
@@ -2,13 +2,13 @@
 
 include_once 'templates.php';
 
-$sablona = "onecolumn";
-$pagetitle = "Write me! Yes!";
+$title = "Write me! Yes!";
 $obsah = '
 <img class="img-ja" src="img/ja_100.jpg" srcset="img/ja_100.jpg, img/ja_200.jpg" width="100" height="100" alt="Tomáš Vykoukal">
 <h1>Contact</h1>
 <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur bibendum justo non orci. Vestibulum fermentum tortor id mi.</p>';
 
-template_contact($pagetitle, $obsah);
+template_head($title);
+template_body($obsah);
 
 ?>
--- templates.php
+++ templates.php
@@ -1,8 +1,8 @@
 <?php
 
-function template_home($title, $obsah) { ?>
+function template_head($title) { ?>
 
-    <!DOCTYPE html>
+<!DOCTYPE html>
     <html lang="cs">
         <head>
             <meta charset="UTF-8">
@@ -41,88 +41,9 @@ function template_home($title, $obsah) { ?>
             </script>
         </head>
 
-        <body>
-            <nav id="menu" role="navigation">
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">About us</a>
-                        <ul>
-                            <li><a href="/about/history">History</a></li>
-                            <li><a href="/about/team">The team</a></li>
-                            <li><a href="/about/address">Our address</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="/contact">Contact</a></li>
-                </ul>
-            </nav>
+<?php }
 
-            <div id="page">
-                <div id="darker"></div>
-                <div class="container h-100 flex flex-column center">
-
-                    <header class="sm-d-flex flex_align--center padding-15" id="header">
-                        <div class="margin_right--auto padding-tb-10" id="logo">
-                            <a href="/">
-                                <img class="inline-block-center" src="img/tomas-vykoukal.png" alt="Tomáš Vykoukal">
-                                <h1 class="inline-block-center">Tomáš Vykoukal</h1>
-                            </a>
-                        </div>
-                        <div class="padding-tb-10" id="menu-toogle" >
-                            <div id="nav-icon3">
-                                <span></span>
-                                <span></span>
-                                <span></span>
-                                <span></span>
-                            </div>
-                        </div>
-                    </header>
-
-                    <article class="row margin_left-right--auto margin_top-bottom--auto padding-15 padding-tb-20" id="main-content-front">
-                        <?php echo $obsah; ?>
-                    </article>
-
-                    <footer class="padding-10" role="contentinfo">
-                        <p>© <?php echo date("Y"); ?> created with my youngest sister and oldest grandma by <abbr title="papa means Italian father">papa</abbr> Tomas</p>
-                        <p><?php echo parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH); ?></p>
-                    </footer>
-                </div>
-            </div>
-        </body>
-    </html>
-    <?php
-}
-
-function template_contact($title, $obsah) {
-    ?>
-
-    <!DOCTYPE html>
-    <html lang="cs">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-            <title><?php echo $title ?></title>
-
-            <link rel="stylesheet" href="/css/normalize.css">
-            <link rel="stylesheet" href="/css/burger-menu.css">
-            <link rel="stylesheet" href="/css/custom.css">
-            <link href="https://fonts.googleapis.com/css?family=Slabo+27px" rel="stylesheet">
-
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-            <script>
-                $(document).ready(function(){
-                    $("#nav-icon3").click(function(){
-                        $("body").toggleClass("sidemenu--open");
-                    });
-
-                    $("#darker").click(function(){
-                        $("body").removeClass("sidemenu--open");
-                        $("#nav-icon3").removeClass("open");
-                    });
-                });
-            </script>
-        </head>
+function template_body($obsah) { ?>
 
         <body>
             <nav id="menu" role="navigation">
@@ -164,12 +85,19 @@ function template_contact($title, $obsah) {
                         <?php echo $obsah; ?>
                     </article>
 
-                    <footer class="padding-15" role="contentinfo">
-                        <p>© <?php echo date("Y"); ?> created with my youngest sister and oldest grandma by <abbr title="papa means Italian father">papa</abbr> Tomas</p>
-                    </footer>
+                    <?php template_footer(); ?>
                 </div>
             </div>
         </body>
     </html>
     <?php
 }
+
+function template_footer() { ?>
+
+    <footer class="padding-10" role="contentinfo">
+        <p>© <?php echo date("Y"); ?> created with my youngest sister and oldest grandma by <abbr title="papa means Italian father">papa</abbr> Tomas</p>
+        <p><?php echo parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH); ?></p>
+    </footer>
+
+<?php }
