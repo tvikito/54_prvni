b5c661b203bf5c18ceb324d70d6a4536bff56471
Tomáš Vykoukal <vykoukal.tom@gmail.com>
2018-04-12 18:36:52 +0200

    New layout (#6)
    * few changes
    
    * commit

--- contact.php
+++ contact.php
@@ -4,14 +4,14 @@ include_once 'templates.php';
 
 $title = "Write me! Yes!";
 
-$desc = '';
+$desc = 'Everything you need to know about the topic you are looking for';
 
-$obsah = '
-<img class="img-ja" src="/img/ja_100.jpg" srcset="/img/ja_100.jpg, /img/ja_200.jpg" width="100" height="100" alt="Tomáš Vykoukal">
-<h1>Contact</h1>
-<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur bibendum justo non orci. Vestibulum fermentum tortor id mi.</p>';
+template_head($title, $desc); ?>
 
-template_head($title, $desc);
-template_body($obsah);
+<article>
+    <img class="img-ja" src="/img/ja_100.jpg" srcset="/img/ja_100.jpg, /img/ja_200.jpg" width="100" height="100" alt="Tomáš Vykoukal">
+    <h1>Contact me!</h1>
+    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur bibendum justo non orci. Vestibulum fermentum tortor id mi.</p>
+</article>
 
-?>
+<?php template_footer();
--- head.php
+++ head.php
@@ -1,7 +1,3 @@
-<?php
-
-function template_head($title = null, $desc = null) { ?>
-
 <!DOCTYPE html>
     <html lang="cs">
         <head>
@@ -47,54 +43,3 @@ function template_head($title = null, $desc = null) { ?>
                 });
             </script>
         </head>
-
-        <body>
-            <nav id="menu" role="navigation">
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">About us</a>
-                        <ul>
-                            <li><a href="/about/history">History</a></li>
-                            <li><a href="/about/team">The team</a></li>
-                            <li><a href="/about/address">Our address</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="/contact.php">Contact</a></li>
-                </ul>
-            </nav>
-
-            <div id="page">
-                <div id="darker"></div>
-                <div class="container h-100 flex flex-column center">
-
-                    <header class="sm-d-flex flex_align--center padding-15" id="header">
-                        <div class="margin_right--auto padding-tb-10" id="logo">
-                            <a href="/">
-                                <img class="inline-block-center" src="/img/tomas-vykoukal.png" alt="Tomáš Vykoukal">
-                                <h1 class="inline-block-center">Tomáš Vykoukal</h1>
-                            </a>
-                        </div>
-                        <div class="padding-tb-10" id="menu-toogle" >
-                            <div id="nav-icon3">
-                                <span></span>
-                                <span></span>
-                                <span></span>
-                                <span></span>
-                            </div>
-                        </div>
-                    </header>
-
-                    <section class="row margin_left-right--auto margin_top-bottom--auto padding-15 padding-tb-20" id="main-content-front">
-<?php }
-
-function template_footer() { ?>
-                    </section>
-
-                    <footer class="padding-10" role="contentinfo">
-                        <p><small>© <?php echo date("Y"); ?> created with my youngest sister and oldest grandma<br>by <abbr title="papa means Italian father">papa</abbr> Tomas</small></p>
-                    </footer>
-                </div>
-            </div>
-        </body>
-    </html>
-<?php }
